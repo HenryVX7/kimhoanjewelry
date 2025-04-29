0fda239b407136431e64e1f4ad561acecdc81f79
Henry <danktroopervx@gmail.com>
2025-04-28 22:51:56 -0700

    viet test

--- general.js
+++ general.js
@@ -6,7 +6,6 @@ function toggleMenu() {
     nav.classList.toggle('open');
 }
 
-
 function openModal(src) {
     const modal = document.getElementById('imageModal');
     const modalImg = document.getElementById('modalImg');
@@ -21,6 +20,25 @@ function closeModal() {
 document.addEventListener('keydown', function(event) {
     if (event.key === "Escape") {
       closeModal();
+    }
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var toggleButton = document.getElementById('language-toggle');
+  
+    if (toggleButton) {
+      toggleButton.addEventListener('click', function() {
+        var englishText = document.getElementById('english');
+        var vietnameseText = document.getElementById('vietnamese');
+  
+        if (englishText.style.display === 'none') {
+          englishText.style.display = 'contents';
+          vietnameseText.style.display = 'none';
+        } else {
+          englishText.style.display = 'none';
+          vietnameseText.style.display = 'contents';
+        }
+      });
     }
   });
   
